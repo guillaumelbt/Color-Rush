55bf91088d4485ff0b0f1be3d208b55b7035988e
Guillaume <guill.lambert@orange.fr>
2023-08-23 15:14:08 +0200

    Menu input

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputAction;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] PlayerInput playerInput;
+
+
+    public void Click(CallbackContext ctx)
+    {
+        if (!ctx.canceled) return;
+        if (EventSystem.current.currentSelectedGameObject is not null)
+        {
+            EventSystem.current.currentSelectedGameObject.GetComponent<Button>().onClick.Invoke();
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
     public void Pause()
     {
         Time.timeScale = 0;
